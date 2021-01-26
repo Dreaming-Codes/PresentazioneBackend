45f44271090859da0cbe94b7b8de551eeabb0829
DreamingCodes <lore.rizzotti@gmail.com>
2021-01-26 15:21:44 +0100

    first

---
+++ src/native-test/java/codes/dreaming/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package codes.dreaming;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
